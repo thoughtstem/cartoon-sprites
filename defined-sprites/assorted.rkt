69125dce8f45358c14913a94eb1b51b60da831c2
Sara Lucchini <saralucchini@Stephens-MacBook-Air-5.local>
2019-10-01 17:31:08 -0700

    added ufo sprites

--- defined-sprites/assorted.rkt
+++ defined-sprites/assorted.rkt
@@ -9,39 +9,87 @@
          greenalien-sprite
          pinkalien-sprite
          yellowalien-sprite
-         randomalien-sprite
+         random-alien-sprite
+         beigeufo-sprite
+         blueufo-sprite
+         greenufo-sprite
+         pinkufo-sprite
+         yellowufo-sprite
+         random-ufo-sprite
          )
 
 (require cartoon-assets
-         (only-in game-engine sheet->sprite))
+         (only-in game-engine sheet->sprite
+                              new-sprite)
+         (only-in 2htdp/image rotate
+                              scale
+                              image-width))
+
+;copied from animal-friendly-asset-names
+;pull out elsewhere?
+(define (make-wiggle-animation img)
+    (list img
+          (rotate -10 img)
+          img
+          (rotate 10 img)))
 
 ;aliens
  
 (define beigealien-sprite
-  (sheet->sprite alienbeige-sheet
-                 #:columns 2))
+  (sheet->sprite (scale 0.5 alienbeige-sheet)
+                 #:columns 2
+                 #:delay 4))
  
 (define bluealien-sprite
-  (sheet->sprite alienblue-sheet
-                 #:columns 2))
+  (sheet->sprite (scale 0.5 alienblue-sheet)
+                 #:columns 2
+                 #:delay 4))
 
 (define greenalien-sprite
-  (sheet->sprite aliengreen-sheet
-                 #:columns 2))
+  (sheet->sprite (scale 0.5 aliengreen-sheet)
+                 #:columns 2
+                 #:delay 4))
  
 (define pinkalien-sprite
-  (sheet->sprite alienpink-sheet
-                 #:columns 2))
+  (sheet->sprite (scale 0.5 alienpink-sheet)
+                 #:columns 2
+                 #:delay 4))
 
 (define yellowalien-sprite
-  (sheet->sprite alienyellow-sheet
-                 #:columns 2))
+  (sheet->sprite (scale 0.5 alienyellow-sheet)
+                 #:columns 2
+                 #:delay 4))
 
-(define (randomalien-sprite)
+(define (random-alien-sprite)
   (lambda () (first (shuffle (list beigealien-sprite bluealien-sprite
                                    greenalien-sprite pinkalien-sprite
                                    yellowalien-sprite)))))
- 
+
+;ufos
+
+(define beigeufo-sprite
+  (new-sprite (make-wiggle-animation (scale 0.5 ufobeige)) 5))
+
+(define blueufo-sprite
+  (new-sprite (make-wiggle-animation (scale 0.5 ufoblue)) 5))
+
+(define greenufo-sprite
+  (new-sprite (make-wiggle-animation (scale 0.5 ufogreen)) 5))
+
+(define pinkufo-sprite
+  (new-sprite (make-wiggle-animation (scale 0.5 ufopink)) 5))
+
+(define yellowufo-sprite
+  (new-sprite (make-wiggle-animation (scale 0.5 ufoyellow)) 5))
+
+(define (random-ufo-sprite)
+  (lambda () (first (shuffle (list beigeufo-sprite blueufo-sprite
+                                   greenufo-sprite pinkufo-sprite
+                                   yellowufo-sprite)))))
+
+;spaceships
+
+
 
  ;SPACE -- S (moons/planets)
    ;cursors
